4d5cbacf7a2affe029e71701ab4c4d7406e5edba
linwj <1745727332@qq.com>
2022-03-17 17:43:49 +0800

    web server base knowledge

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@
 const http = require('http');
 
 const server = http.createServer((request,response) => {
+
+
+
+
+  
+  switch(request.url){
+
+  }
+  //查看请求header的user-agent
+  console.log(request.headers['user-agent'])
+  
+  //规定响应头的Content-Type，客户端会根据这个确定返回内容的显示格式
+  response.writeHead(200,{
+    'Content-Type': 'text/html'
+  })
   response.write('hello');
   response.end();
 })
